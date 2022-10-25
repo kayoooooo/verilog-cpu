060373759890393bb609112a876bed116a9050f3
kayoooo <k.yao.kayoo@gmail.com>
2022-10-25 14:51:11 -0400

    made a 4bit fulladder, doesnt work though

--- cpu.v
+++ cpu.v
@@ -13,14 +13,26 @@ module fullAdder(ain, bin,cin, c, s);
 	assign c = (ain & bin) | ((ain ^ bin) & cin) ;  
 endmodule
 
+module fullAdder_4bit(ain, bin, c, s);
+	input [3:0]ain,bin;
+	wire c1,c2,c3;
+	output [3:0] s;
+	output c;
+	halfAdder h0 (ain[0], bin[0], c1, s[0]);
+	fullAdder f0 (ain[1], bin[1], c1, c2, s[1]);
+	fullAdder f1 (ain[2], bin[2], c2, c3, s[2]);
+	fullAdder f2 (ain[3], bin[3], c3, c, s[3]);
+endmodule
+
 module main;
-	reg ain, bin, cin;
-	wire c,s;
+	reg [3:0] ain, bin, cin;
+	wire [3:0] s;
+	wire c;
 	//halfAdder h0 (a,b,c,s);
-	fullAdder f0 (ain, bin, cin, c, s); 
+	fullAdder_4bit f0 (ain, bin, c, s); 
 	initial begin;
-	ain = 1;
-	bin = 1;
+	ain = 0101;
+	bin = 0011;
 	cin = 0;
 	#10
 	$display(s);
